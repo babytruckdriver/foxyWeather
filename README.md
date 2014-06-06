abd2ccb41617dfe0299c8a828b2300d0e8eaf41b
babytruckdriver <babytruckdriver@gmail.com>
2014-06-07 00:13:56 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Foxy Weather
+===========
+
+A foxy weather app for Firefox OS.
+
+Installation
+------------
+
+Just install the app in your foxy phone and play!
---
+++ javascripts/main.js
@@ -0,0 +1,21 @@
+/*jslint indent:8, devel:true, browser:true, vars:true*/
+/*global require*/
+
+require.config({
+        baseUrl: "javascripts",
+        paths: {
+                jquery: "vendor/jquery-2.1.0",
+                handlebars: "vendor/handlebars-v1.3.0"
+        },
+        shim: { //Se configuran los módulos no AMD
+                handlebars: {
+                        exports: "Handlebars"
+                }
+        }
+});
+
+require(["clientApp"], function (Application) {
+        "use strict";
+
+        Application.App.init();
+});
